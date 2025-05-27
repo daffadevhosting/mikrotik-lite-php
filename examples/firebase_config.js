a6daea44a21073d81fc9e0ef2a148262e5704ebb
Daffa Aditya <79897029+daffadevhosting@users.noreply.github.com>
2025-05-27 17:37:04 +0700

    Create firebase_config.js

---
+++ examples/firebase_config.js
@@ -0,0 +1,10 @@
+// Jangan upload file ini ke public repo
+const firebaseConfig = {
+  apiKey: "your_api_key",
+  authDomain: "your_project.firebaseapp.com",
+  databaseURL: "https://your_project.firebaseio.com",
+  projectId: "your_project",
+  storageBucket: "your_project.appspot.com",
+  messagingSenderId: "your_sender_id",
+  appId: "your_app_id"
+};
